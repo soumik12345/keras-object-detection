00763836ffc5e9af5ac5c6c04205822557b7bce0
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2021-11-13 22:08:08 +0530

    feedback round 1

--- keras_object_detection/dataloader/augmentations.py
+++ keras_object_detection/dataloader/augmentations.py
@@ -4,7 +4,9 @@ import tensorflow as tf
 def random_flip_data(image, boxes, classes):
     w = tf.cast(tf.shape(image)[1], dtype=tf.float32)
     if tf.random.uniform(()) > 0.5:
+        # Horizonatlly flipping the image
         image = tf.image.flip_left_right(image)
+        # Horizonatlly flipping the boxes
         boxes = tf.stack(
             [w - boxes[:, 2], boxes[:, 1], w - boxes[:, 0], boxes[:, 3]], axis=-1
         )
